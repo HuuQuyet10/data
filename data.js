f7d1af7103ce1abc3559f6b5716edc0db6a99533
Quyet Pham <31075842+HuuQuyet10@users.noreply.github.com>
2020-03-24 17:58:00 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,354 @@
+[
+  {
+    "id": "3",
+    "duan": "duan 3",
+    "sanpham": "sanpham 3",
+    "congviec": "congviec 3",
+    "giobatdau": 1584951369,
+    "gioketthuc": 1584951369
+  },
+  {
+    "id": "4",
+    "duan": "duan 4",
+    "sanpham": "sanpham 4",
+    "congviec": "congviec 4",
+    "giobatdau": 1584951309,
+    "gioketthuc": 1584951309
+  },
+  {
+    "id": "5",
+    "duan": "duan 5",
+    "sanpham": "sanpham 5",
+    "congviec": "congviec 5",
+    "giobatdau": 1584951249,
+    "gioketthuc": 1584951249
+  },
+  {
+    "id": "6",
+    "duan": "duan 6",
+    "sanpham": "sanpham 6",
+    "congviec": "congviec 6",
+    "giobatdau": 1585026148,
+    "gioketthuc": 1585026148
+  },
+  {
+    "id": "7",
+    "duan": "duan 7",
+    "sanpham": "sanpham 7",
+    "congviec": "congviec 7",
+    "giobatdau": 1585026088,
+    "gioketthuc": 1585026088
+  },
+  {
+    "id": "8",
+    "duan": "duan 8",
+    "sanpham": "sanpham 8",
+    "congviec": "congviec 8",
+    "giobatdau": 1585026028,
+    "gioketthuc": 1585026028
+  },
+  {
+    "id": "9",
+    "duan": "duan 9",
+    "sanpham": "sanpham 9",
+    "congviec": "congviec 9",
+    "giobatdau": 1585025968,
+    "gioketthuc": 1585025968
+  },
+  {
+    "id": "10",
+    "duan": "duan 10",
+    "sanpham": "sanpham 10",
+    "congviec": "congviec 10",
+    "giobatdau": 1585025908,
+    "gioketthuc": 1585025908
+  },
+  {
+    "id": "11",
+    "duan": "duan 11",
+    "sanpham": "sanpham 11",
+    "congviec": "congviec 11",
+    "giobatdau": 1585025848,
+    "gioketthuc": 1585025848
+  },
+  {
+    "id": "13",
+    "duan": "duan 13",
+    "sanpham": "sanpham 13",
+    "congviec": "congviec 13",
+    "giobatdau": 1585025728,
+    "gioketthuc": 1585025728
+  },
+  {
+    "id": "15",
+    "duan": "duan 15",
+    "sanpham": "sanpham 15",
+    "congviec": "congviec 15",
+    "giobatdau": 1585025608,
+    "gioketthuc": 1585025608
+  },
+  {
+    "id": "16",
+    "duan": "duan 16",
+    "sanpham": "sanpham 16",
+    "congviec": "congviec 16",
+    "giobatdau": 1585025548,
+    "gioketthuc": 1585025548
+  },
+  {
+    "id": "17",
+    "duan": "duan 17",
+    "sanpham": "sanpham 17",
+    "congviec": "congviec 17",
+    "giobatdau": 1585025488,
+    "gioketthuc": 1585025488
+  },
+  {
+    "id": "18",
+    "duan": "duan 18",
+    "sanpham": "sanpham 18",
+    "congviec": "congviec 18",
+    "giobatdau": 1585025428,
+    "gioketthuc": 1585025428
+  },
+  {
+    "id": "19",
+    "duan": "duan 19",
+    "sanpham": "sanpham 19",
+    "congviec": "congviec 19",
+    "giobatdau": 1585025368,
+    "gioketthuc": 1585025368
+  },
+  {
+    "id": "20",
+    "duan": "duan 20",
+    "sanpham": "sanpham 20",
+    "congviec": "congviec 20",
+    "giobatdau": 1585025308,
+    "gioketthuc": 1585025308
+  },
+  {
+    "id": "21",
+    "duan": "duan 21",
+    "sanpham": "sanpham 21",
+    "congviec": "congviec 21",
+    "giobatdau": 1585025248,
+    "gioketthuc": 1585025248
+  },
+  {
+    "id": "22",
+    "duan": "duan 22",
+    "sanpham": "sanpham 22",
+    "congviec": "congviec 22",
+    "giobatdau": 1585025188,
+    "gioketthuc": 1585025188
+  },
+  {
+    "id": "23",
+    "duan": "duan 23",
+    "sanpham": "sanpham 23",
+    "congviec": "congviec 23",
+    "giobatdau": 1585025130,
+    "gioketthuc": 1585025130
+  },
+  {
+    "id": "24",
+    "duan": "duan 24",
+    "sanpham": "sanpham 24",
+    "congviec": "congviec 24",
+    "giobatdau": 1585025070,
+    "gioketthuc": 1585025070
+  },
+  {
+    "id": "25",
+    "duan": "duan 25",
+    "sanpham": "sanpham 25",
+    "congviec": "congviec 25",
+    "giobatdau": 1585025010,
+    "gioketthuc": 1585025010
+  },
+  {
+    "id": "26",
+    "duan": "duan 26",
+    "sanpham": "sanpham 26",
+    "congviec": "congviec 26",
+    "giobatdau": 1585024950,
+    "gioketthuc": 1585024950
+  },
+  {
+    "id": "27",
+    "duan": "duan 27",
+    "sanpham": "sanpham 27",
+    "congviec": "congviec 27",
+    "giobatdau": 1585024890,
+    "gioketthuc": 1585024890
+  },
+  {
+    "id": "28",
+    "duan": "duan 28",
+    "sanpham": "sanpham 28",
+    "congviec": "congviec 28",
+    "giobatdau": 1585024830,
+    "gioketthuc": 1585024830
+  },
+  {
+    "id": "29",
+    "duan": "duan 29",
+    "sanpham": "sanpham 29",
+    "congviec": "congviec 29",
+    "giobatdau": 1585024770,
+    "gioketthuc": 1585024770
+  },
+  {
+    "id": "30",
+    "duan": "duan 30",
+    "sanpham": "sanpham 30",
+    "congviec": "congviec 30",
+    "giobatdau": 1585024710,
+    "gioketthuc": 1585024710
+  },
+  {
+    "id": "31",
+    "duan": "duan 31",
+    "sanpham": "sanpham 31",
+    "congviec": "congviec 31",
+    "giobatdau": 1585024650,
+    "gioketthuc": 1585024650
+  },
+  {
+    "id": "32",
+    "duan": "duan 32",
+    "sanpham": "sanpham 32",
+    "congviec": "congviec 32",
+    "giobatdau": 1585024590,
+    "gioketthuc": 1585024590
+  },
+  {
+    "id": "33",
+    "duan": "duan 33",
+    "sanpham": "sanpham 33",
+    "congviec": "congviec 33",
+    "giobatdau": 1585024530,
+    "gioketthuc": 1585024530
+  },
+  {
+    "id": "34",
+    "duan": "duan 34",
+    "sanpham": "sanpham 34",
+    "congviec": "congviec 34",
+    "giobatdau": 1585024470,
+    "gioketthuc": 1585024470
+  },
+  {
+    "id": "35",
+    "duan": "duan 35",
+    "sanpham": "sanpham 35",
+    "congviec": "congviec 35",
+    "giobatdau": 1585024411,
+    "gioketthuc": 1585024411
+  },
+  {
+    "id": "36",
+    "duan": "duan 36",
+    "sanpham": "sanpham 36",
+    "congviec": "congviec 36",
+    "giobatdau": 1585024351,
+    "gioketthuc": 1585024351
+  },
+  {
+    "id": "37",
+    "duan": "duan 37",
+    "sanpham": "sanpham 37",
+    "congviec": "congviec 37",
+    "giobatdau": 1585024291,
+    "gioketthuc": 1585024291
+  },
+  {
+    "id": "38",
+    "duan": "duan 38",
+    "sanpham": "sanpham 38",
+    "congviec": "congviec 38",
+    "giobatdau": 1585024231,
+    "gioketthuc": 1585024231
+  },
+  {
+    "id": "39",
+    "duan": "duan 39",
+    "sanpham": "sanpham 39",
+    "congviec": "congviec 39",
+    "giobatdau": 1585044391,
+    "gioketthuc": 1585044391
+  },
+  {
+    "id": "40",
+    "duan": "duan 40",
+    "sanpham": "sanpham 40",
+    "congviec": "congviec 40",
+    "giobatdau": 1585044331,
+    "gioketthuc": 1585044331
+  },
+  {
+    "id": "41",
+    "duan": "duan 41",
+    "sanpham": "sanpham 41",
+    "congviec": "congviec 41",
+    "giobatdau": 1585044271,
+    "gioketthuc": 1585044271
+  },
+  {
+    "id": "42",
+    "duan": "duan 42",
+    "sanpham": "sanpham 42",
+    "congviec": "congviec 42",
+    "giobatdau": 1585044211,
+    "gioketthuc": 1585044211
+  },
+  {
+    "id": "43",
+    "duan": "duan 43",
+    "sanpham": "sanpham 43",
+    "congviec": "congviec 43",
+    "giobatdau": 1585044151,
+    "gioketthuc": 1585044151
+  },
+  {
+    "id": "44",
+    "duan": "duan 44",
+    "sanpham": "sanpham 44",
+    "congviec": "congviec 44",
+    "giobatdau": 1585044091,
+    "gioketthuc": 1585044091
+  },
+  {
+    "id": "45",
+    "duan": "duan 45",
+    "sanpham": "sanpham 45",
+    "congviec": "congviec 45",
+    "giobatdau": 1585044031,
+    "gioketthuc": 1585044031
+  },
+  {
+    "id": "46",
+    "duan": "duan 46",
+    "sanpham": "sanpham 46",
+    "congviec": "congviec 46",
+    "giobatdau": 1585043971,
+    "gioketthuc": 1585043971
+  },
+  {
+    "id": "47",
+    "duan": "duan 47",
+    "sanpham": "sanpham 47",
+    "congviec": "congviec 47",
+    "giobatdau": 1585043911,
+    "gioketthuc": 1585043911
+  },
+  {
+    "id": "48",
+    "duan": "duan 48",
+    "sanpham": "sanpham 48",
+    "congviec": "congviec 48",
+    "giobatdau": 1585043851,
+    "gioketthuc": 1585043851
+  }
+]
